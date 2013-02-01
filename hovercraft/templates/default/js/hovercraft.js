a9eb6f3668295ab6d8544b82120a1b32ef2b804e
Lennart Regebro <regebro@gmail.com>
2013-02-01 13:25:01 +0100

    Drop plans for "Show source" and "Show slide numbers" functionality. I don't need it. Landslide has it, and it's neat, but it can wait until someone wants it.

--- hovercraft/templates/default/js/hovercraft.js
+++ hovercraft/templates/default/js/hovercraft.js
@@ -1,30 +1,7 @@
-var showSlideNumbers = function() {
-    var asides = document.getElementsByClassName('page_number');
-    var hidden = asides[0].style.display != 'block';
-    for (var i = 0; i < asides.length; i++) {
-        asides.item(i).style.display = hidden ? 'block' : 'none';
-    }
-};
-
-var showSlideSources = function() {
-    var asides = document.getElementsByClassName('source');
-    var hidden = asides[0].style.display != 'block';
-    for (var i = 0; i < asides.length; i++) {
-        asides.item(i).style.display = hidden ? 'block' : 'none';
-    }
-};
-
 impress().init();
 console().init(cssFile='css/impressConsole.css');
 
 var impressattrs = document.getElementById('impress').attributes
 if (impressattrs.hasOwnProperty('auto-console') && impressattrs['auto-console'].value === 'True') {
     consoleWindow = console().open();
-    /*
-        
-        console().registerKeyEvent([78], showSlideNumbers);
-        console().registerKeyEvent([83], showSlideSources); */
 }
-
-console().registerKeyEvent([78], showSlideNumbers, window);
-console().registerKeyEvent([83], showSlideSources, window);
--- hovercraft/tests/test_data/maximal/js/hovercraft.js
+++ hovercraft/tests/test_data/maximal/js/hovercraft.js
@@ -1,24 +1,7 @@
-var showSlideNumbers = function() {
-    var asides = document.getElementsByClassName('page_number');
-    var hidden = asides[0].style.display != 'block';
-    for (var i = 0; i < asides.length; i++) {
-        asides.item(i).style.display = hidden ? 'block' : 'none';
-    }
-};
-
-var showSlideSources = function() {
-    var asides = document.getElementsByClassName('source');
-    var hidden = asides[0].style.display != 'block';
-    for (var i = 0; i < asides.length; i++) {
-        asides.item(i).style.display = hidden ? 'block' : 'none';
-    }
-};
-
 impress().init();
-console().init(cssFile="{{theme_dir}}/css/impressConsole.css");
-//console().open();
+console().init(cssFile='css/impressConsole.css');
 
-console().registerKeyEvent([78], showSlideNumbers)
-console().registerKeyEvent([78], showSlideNumbers, window)
-console().registerKeyEvent([83], showSlideSources)
-console().registerKeyEvent([83], showSlideSources, window)
+var impressattrs = document.getElementById('impress').attributes
+if (impressattrs.hasOwnProperty('auto-console') && impressattrs['auto-console'].value === 'True') {
+    consoleWindow = console().open();
+}
