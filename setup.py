dd19c1d26d38003467b8ecf85490f6b680ff95e0
Lennart Regebro <regebro@gmail.com>
2016-02-27 22:20:27 -0500

    Preparing release 2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 import sys
 
-version = '2.1.dev0'
+version = '2.1'
 
 with open('README.rst', 'rt') as readme:
     description = readme.read()
