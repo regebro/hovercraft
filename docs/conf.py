b3146d3445e6c7115b8e20367817aa05b3624152
Lennart Regebro <regebro@gmail.com>
2017-10-12 10:26:34 -0400

    There's a bug in Sphinx, used by read the docs
    This measn Sphinx can't render the tutorial.rst.

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ release = '1.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = ['examples']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
