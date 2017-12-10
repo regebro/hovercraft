0b3fd8d993a1e7c0ca5812f7b1d2091443c00687
Lennart Regebro <regebro@gmail.com>
2017-12-10 13:36:35 +0100

    Back to development: 3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 import sys
 
-version = '2.5'
+version = '3.0.dev0'
 
 with open('README.rst', 'rt', encoding='utf8') as readme:
     description = readme.read()
