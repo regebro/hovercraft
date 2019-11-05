e0aca20c1d78e98297f937def56958bd4bd67aa9
Lennart <regebro@gmail.com>
2019-11-05 20:45:31 +0100

    Since pip>=9.0.0 you can sepcify the Python version
    So this ugly hack is no longer needed

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
 from setuptools import setup
-from setuptools.command.install import install
-import sys
 
-class CustomInstall(install):
-    def initialize_options(self):
-        if sys.version < "3":
-            print("Hovercraft requires Python 3.5 or higher.")
-            sys.exit(1)
-
-        return install.initialize_options(self)
-
-setup(cmdclass={"install": CustomInstall})
+setup()
